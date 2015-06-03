9aed52e3ef705e138855e11ee9994df8e6f404a8
Olivier Yiptong <olivier@olivieryiptong.com>
2015-06-03 16:13:59 -0400

    skeleton new newtab page

---
+++ data/js/newnewtab.js
@@ -0,0 +1,7 @@
+self.port.on("style", function(file) {
+  let link = document.createElement("link");
+  link.setAttribute("href", file);
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  document.head.appendChild(link);
+});
