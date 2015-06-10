4bc2047ba24452f72d896a174fdb2eb2027dadbb
Olivier Yiptong <olivier@olivieryiptong.com>
2015-06-10 03:29:50 -0400

    add demo feed aggregation and display

--- chrome/content/js/newnewtab.js
+++ chrome/content/js/newnewtab.js
@@ -64,10 +64,49 @@ var Tile = React.createClass({
   }
 });
 
+var TileRSS = React.createClass({
+  getDefaultProps: function() {
+    return {
+      url: '',
+      title: '',
+      directoryId: null,
+      frecency: null,
+      imageURI: '',
+      enhancedImageURI: null,
+      lastVisitDate: null,
+      bgColor: null,
+      type: '',
+      entries: []
+    }
+  },
+
+  render: function() {
+    var rows = this.props.tile.entries.map(function(entry) {
+      return <li><a href={entry.link}>{entry.title}</a></li>
+    }.bind(this));
+
+    return (
+        <div className="newtab-cell">
+          <div className="newtab-site">
+            <div className="newtab-link" title={this.props.tile.title}>
+              <div className="newtab-rss">
+                <ul>
+                  {rows}
+                </ul>
+              </div>
+              <span className="newtab-title">{this.props.tile.title}</span>
+            </div>
+          </div>
+        </div>
+    );
+  }
+});
+
 var TileGrid = React.createClass({
   getInitialState: function() {
     return {
-      tiles: []
+      tiles: [],
+      feedEntries: {}
     };
   },
 
@@ -82,17 +121,30 @@ var TileGrid = React.createClass({
         this.setState({tiles: items});
       }
     }.bind(this));
+
+    addon.on("feedData", function(data) {
+      if (this.isMounted()) {
+        this.setState(function(prevState, props) {
+          prevState.feedEntries[data.url] = data;
+          return prevState;
+        });
+        this.forceUpdate();
+      }
+    }.bind(this));
+
     addon.emit("gridReady");
   },
 
   render: function() {
-    var tiles = this.state.tiles;
+    var tiles = this.state.tiles.map(function(tile) {
+      if (this.state.feedEntries.hasOwnProperty(tile.url)) {
+        return <TileRSS tile={this.state.feedEntries[tile.url]} key={tile.directoryId}/>;
+      }
+      return <Tile tile={tile} key={tile.directoryId}/>;
+    }.bind(this));
+
     return (
-      <div id="newtab-grid">
-        {tiles.map(function(tile) {
-          return <Tile tile={tile} key={tile.directoryId}/>;
-        })}
-      </div>
+      <div id="newtab-grid">{tiles}</div>
     );
   }
 });
